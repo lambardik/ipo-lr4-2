56114c66b7af2bc7b166c661518f592bd0f5b573
даниэль <dannilboyar@gmail.com>
2024-10-24 10:09:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def square_list(numbers):
-  
-  return [x**2 for x in numbers]
-
 numbers = [1, 20, 36, 4, 5]
-squares = square_list(numbers)
-print(squares) 
+squares = [x**2 for x in numbers]  
+print(squares)
